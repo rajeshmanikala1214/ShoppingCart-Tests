0c3d8d89719dfa1806f0e07521a4b3e46882cc1e
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-05-22 18:52:15 +0530

    changed alltests and alljournreys according to the claude and recording

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,5 +1,5 @@
 // webapp/test/integration/AllJourneys.js
-sap.ui.define([
+sap.ui.require([
     "sap/ui/test/Opa5",
     "sap/ui/demo/cart/test/integration/arrangements/component/Startup",
     "sap/ui/demo/cart/test/integration/WelcomeJourney",
@@ -9,11 +9,11 @@ sap.ui.define([
     "sap/ui/demo/cart/test/integration/PhoneNavigationJourney",
     "sap/ui/demo/cart/test/integration/BuyProductJourney",
     "sap/ui/demo/cart/test/integration/DeleteProductJourney"
-], function (Opa5, Startup) {
+], function(Opa5, Startup) {
     "use strict";
     Opa5.extendConfig({
-        arrangements: new Startup(),
+        arrangements:  new Startup(),
         viewNamespace: "sap.ui.demo.cart.view.",
-        autoWait: true
+        autoWait:      true
     });
 });
