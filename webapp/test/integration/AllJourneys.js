364f11f876a87a23019c6775458599be17350cb5
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-05-29 23:37:24 +0530

    changes in AllTests and AllJourneys

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,5 +1,5 @@
 // webapp/test/integration/AllJourneys.js
-sap.ui.require([
+sap.ui.define([
     "sap/ui/test/Opa5",
     "sap/ui/demo/cart/test/integration/arrangements/component/Startup",
     "sap/ui/demo/cart/test/integration/WelcomeJourney",
@@ -9,7 +9,7 @@ sap.ui.require([
     "sap/ui/demo/cart/test/integration/PhoneNavigationJourney",
     "sap/ui/demo/cart/test/integration/BuyProductJourney",
     "sap/ui/demo/cart/test/integration/DeleteProductJourney"
-], function(Opa5, Startup) {
+], function (Opa5, Startup) {
     "use strict";
     Opa5.extendConfig({
         arrangements:  new Startup(),
--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,9 +1,11 @@
 // webapp/test/unit/AllTests.js
-sap.ui.require([
+sap.ui.define([
     "sap/ui/demo/cart/test/unit/controller/Checkout.controller",
     "sap/ui/demo/cart/test/unit/helper/FakeI18nModel",
     "sap/ui/demo/cart/test/unit/model/EmailType",
     "sap/ui/demo/cart/test/unit/model/formatter",
     "sap/ui/demo/cart/test/unit/model/LocalStorageModel",
     "sap/ui/demo/cart/test/unit/model/models"
-]);
+], function () {
+    "use strict";
+});
